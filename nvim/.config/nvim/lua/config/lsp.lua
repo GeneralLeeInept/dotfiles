2c8bb6d7478c3bd2c9b95e4c7cfb20e9d6456ae2
Derek Burnheim <derek@hemicube.com>
2025-10-18 22:33:19 +1100

    nvim - bring back mason to install rust-analyzer

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -4,6 +4,6 @@ vim.lsp.enable('rust-analyzer')
 vim.diagnostic.config({ virtual_text = true })
 
 vim.keymap.set('n', "K", vim.lsp.buf.hover, {})
-vim.keymap.set('n', "<leader>bf", vim.lsp.buf.format, {})
 vim.keymap.set('n', 'gd', vim.lsp.buf.type_definition, {})
+vim.keymap.set('n', "<leader>f", vim.lsp.buf.format, {})
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,4 +4,4 @@ vim.o.relativenumber = true
 vim.o.shiftwidth = 4
 vim.o.wrap = false
 vim.o.expandtab = true
- 
+vim.o.winborder = 'rounded'
